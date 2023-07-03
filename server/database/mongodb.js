85ec084a47f6daa6bc96b9bfd8a781f6be200913
Arjun Kumar <93002735+arjunroy7979@users.noreply.github.com>
2023-07-03 16:22:51 +0530

    Update mongodb.js

--- server/database/mongodb.js
+++ server/database/mongodb.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export default async function ConnectionDB() {
-    mongoose.connect('mongodb+srv://admin:admin@cluster0.kxfrlrn.mongodb.net/Employee')
+    mongoose.connect('mongodb://127.0.0.1:27017/Employee')
         .then((res) => console.log("Database connected"))
         .catch((err) => console.log(err))
 }
